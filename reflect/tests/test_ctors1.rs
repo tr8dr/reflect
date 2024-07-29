09890bbe75b283dc1a8540b3cdeb381c48c71d18
jshore <jonathan.shore@gmail.com>
2024-07-29 14:05:30 -0400

    got rid of tests crate

--- reflect/tests/test_ctors1.rs
+++ reflect/tests/test_ctors1.rs
@@ -19,22 +19,18 @@ impl Test1 {
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
 
-    #[test]
-    fn test_ctors1() {
-        let args = &[
-            Box::new(3i32) as Box<dyn Any>,
-            Box::new(3.1415926f64) as Box<dyn Any>
-        ];
-        let itype = TypeInfo::find_type(&String::from("Test1")).expect("could not find type");
+#[test]
+fn test_ctors1() {
+    let args = &[
+        Box::new(3i32) as Box<dyn Any>,
+        Box::new(3.1415926f64) as Box<dyn Any>
+    ];
+    let itype = TypeInfo::find_type(&String::from("Test1")).expect("could not find type");
 
-        //let rawobj = reflect_create("Test1", args).expect("failed to call ctor");
-        //let obj = rawobj.downcast_ref::<Test1>().expect("faied to downcast to type");
+    //let rawobj = reflect_create("Test1", args).expect("failed to call ctor");
+    //let obj = rawobj.downcast_ref::<Test1>().expect("faied to downcast to type");
 
-        //assert_eq!(obj.alpha, 3);
-        //assert_eq!(obj.beta, 3.1415926);
-    }
+    //assert_eq!(obj.alpha, 3);
+    //assert_eq!(obj.beta, 3.1415926);
 }
