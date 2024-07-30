080e41f38e7ad86e83e84658aa5243f35927ac47
jshore <jonathan.shore@gmail.com>
2024-07-30 16:33:03 -0400

    fixed static argument problems

--- reflect_macros/src/lib.rs
+++ reflect_macros/src/lib.rs
@@ -17,19 +17,49 @@ use syn::{parse_macro_input, ItemImpl, ImplItem, ImplItemMethod, ReturnType, Typ
 use proc_macro2::TokenStream as TokenStream2;
 
 
+/// Types of functions we may encounter in a type
+enum FunctionType {
+    Constructor,
+    Method,
+    Static,
+}
+
+
 /// Attribute to reflect ctors and methods in a type implementation
 ///
 /// # Usage
 /// ```
 /// #[reflect_type]
 /// impl MyType {
-///     fn new (&self, a: f64) -> &Self;
+///     fn new (&self, a: f64, b: f64) -> &Self;
 ///
 ///     fn f (&self, x: f64) -> f64;
 /// }
 /// ```
-/// In the above example the constructor and the method `f` would be registered and made available
-/// for reflection.
+///
+/// This will generate:
+/// - an implementation of Constructor for each ctor
+/// - an implementation of Method for each method
+/// - registration for each ctor and method
+/// - registration for the overall type
+///
+/// Given the above registration can then:
+/// - create new `MyType` through reflection, yielding an object, say `obj`:
+///   * `let obj = TypeInfo.create (args)`
+/// - call methods by name
+///   * `TypeInfo.call (obj, "f", arguments)`
+///
+/// The above is not terribly useful within Rust code, however when paired with a parser, from
+/// configuration, python, etc. could have a constructed expression such as:
+///
+/// In json config
+/// ```
+/// {
+///    "ctor": "MyType(42, 3.1415926)"
+/// }
+/// ```
+/// and use a parser (which we will provide) to construct types, nested types, etc. based on
+/// expressions in configuration or from a scripting environment.
 ///
 #[proc_macro_attribute]
 pub fn reflect_type(_attr: TokenStream, item: TokenStream) -> TokenStream {
@@ -49,10 +79,31 @@ let input = parse_macro_input!(item as ItemImpl);
         panic!("Expected a viable type")
     };
 
+    // Helper function to check if the return type is Self or impl Trait
+    fn is_self_or_impl_trait(ty: &Type) -> bool {
+        match ty {
+            Type::Path(type_path) if type_path.path.is_ident("Self") => true,
+            Type::ImplTrait(_) => true,
+            _ => false,
+        }
+    }
+
     // create code for each ctor or method
     let registrations = input.items.iter().filter_map(|item| {
         if let ImplItem::Method(method) = item {
             let method_name = &method.sig.ident;
+
+            // Determine if this is a constructor, instance method, or static method
+            let function_type = if method.sig.receiver().is_none() {
+                match &method.sig.output {
+                    ReturnType::Type(_, ty) =>
+                        if is_self_or_impl_trait(ty) { FunctionType::Constructor } else { FunctionType::Static },
+                    ReturnType::Default =>
+                        FunctionType::Static,
+                }
+            } else {
+                FunctionType::Method
+            };
 
             // get arguments for function
             let args = method.sig.inputs.iter()
@@ -70,101 +121,113 @@ let input = parse_macro_input!(item as ItemImpl);
                 };
             }).collect::<Vec<_>>();
 
+            // names of arguments
             let arg_names = args.iter().map(|(name, _)| quote! { #name }).collect::<Vec<_>>();
-            let arg_types = args.iter().map(|(_, ty)| quote! { std::any::TypeId::of::<#ty>() }).collect::<Vec<_>>();
-
+            // type ids of arguments
             let (return_type, return_statement) = match &method.sig.output {
                 ReturnType::Default => (quote! { () }, quote! { Ok(Box::new(())) }),
                 ReturnType::Type(_, ty) => (quote! { #ty }, quote! { Ok(Box::new(result)) }),
             };
 
-            let is_ctor = method.sig.receiver().is_none();
-            if is_ctor {
-                let ctor_name = format_ident!("{}Constructor", ident_camel_case(method_name));
-                let register_ident = format_ident!("_REGISTER_{}", ctor_name);
-                Some(quote! {
+            let arg_types = args.iter()
+                .map(|(_, ty)| quote! { std::any::TypeId::of::<#ty>() })
+                .collect::<Vec<_>>();
 
-                    /// specific Constructor type
-                    #[derive(Clone)]
-                    struct #ctor_name {
-                        _arg_types: Vec<std::any::TypeId>
-                    }
+            match function_type {
+                // constructor functions
+                FunctionType::Constructor => {
+                    let ctor_name = format_ident!("{}Constructor", ident_camel_case(method_name));
+                    let register_ident = format_ident!("_REGISTER_{}", ctor_name);
 
-                    /// implementation of Constructor trait for the given ctor
-                    impl ::reflect::Constructor for #ctor_name {
-                        fn create(&self, args: &[Box<dyn std::any::Any>]) -> Result<Box<dyn std::any::Any>, String> {
-                            #(#arg_conversions)*
-                            let result = #short_type_name::#method_name(#(#arg_names),*);
-                            #return_statement
+                    Some(quote! {
+                        /// specific Constructor type
+                        #[derive(Clone)]
+                        struct #ctor_name {
+                            _arg_types: Vec<std::any::TypeId>
                         }
 
-                        fn arg_types(&self) -> &[std::any::TypeId] {
-                            &self._arg_types
+                        /// implementation of Constructor trait for the given ctor
+                        impl ::reflect::Constructor for #ctor_name {
+                            fn create(&self, args: &[Box<dyn std::any::Any>]) -> Result<Box<dyn std::any::Any>, String> {
+                                #(#arg_conversions)*
+                                let result = #short_type_name::#method_name(#(#arg_names),*);
+                                #return_statement
+                            }
+
+                            fn arg_types(&self) -> &[std::any::TypeId] {
+                                &self._arg_types
+                            }
+
+                            fn return_type(&self) -> std::any::TypeId {
+                                std::any::TypeId::of::<#return_type>()
+                            }
+
+                            fn clone_boxed(&self) -> Box<dyn Constructor> {
+                                Box::new(self.clone())
+                            }
                         }
 
-                        fn return_type(&self) -> std::any::TypeId {
-                            std::any::TypeId::of::<#return_type>()
+                        /// auto-registration function
+                        #[ctor::ctor]
+                        fn #register_ident() {
+                            ::reflect::register_constructor::<#short_type_name>(Box::new(#ctor_name {
+                                _arg_types: vec![#(#arg_types),*]
+                            }));
+                        }
+                    })
+                }
+
+                // normal method functions
+                FunctionType::Method => {
+                    let method_impl_name = format_ident!("{}Method", ident_camel_case(method_name));
+                    let register_ident = format_ident!("_REGISTER_{}", method_impl_name);
+
+                    Some(quote! {
+                        /// specific Method type
+                        #[derive(Clone)]
+                        struct #method_impl_name {
+                            _name: String,
+                            _arg_types: Vec<std::any::TypeId>
                         }
 
-                        fn clone_boxed(&self) -> Box<dyn Constructor> {
-                            Box::new(self.clone())
-                        }
-                    }
+                        /// implementation of Method trait for the given method
+                        impl ::reflect::Method for #method_impl_name {
+                            fn call(&self, obj: &Box<dyn std::any::Any>, args: &[Box<dyn std::any::Any>]) -> Result<Box<dyn std::any::Any>, String> {
+                                #(#arg_conversions)*
+                                let realobj = obj.downcast_ref::<#type_path>().expect("Failed to downcast to correct type");
+                                let result = realobj.#method_name(#(#arg_names),*);
+                                #return_statement
+                            }
 
-                    /// auto-registration function
-                    #[ctor::ctor]
-                    fn #register_ident() {
-                        ::reflect::register_constructor::<#short_type_name>(Box::new(#ctor_name {
-                            _arg_types: vec![#(#arg_types),*]
-                        }));
-                    }
-                })
-            } else {
-                let method_impl_name = format_ident!("{}Method", ident_camel_case(method_name));
-                let register_ident = format_ident!("_REGISTER_{}", method_impl_name);
-                Some(quote! {
+                            fn arg_types(&self) -> &[std::any::TypeId] {
+                                &self._arg_types
+                            }
+                            fn return_type(&self) -> std::any::TypeId {
+                                std::any::TypeId::of::<#return_type>()
+                            }
 
-                    /// specific Method type
-                    #[derive(Clone)]
-                    struct #method_impl_name {
-                        _name: String,
-                        _arg_types: Vec<std::any::TypeId>
-                    }
+                            fn name(&self) -> &String {
+                                &self._name
+                            }
 
-                    /// implementation of Method trait for the given method
-                    impl ::reflect::Method for #method_impl_name {
-                        fn call(&self, obj: &Box<dyn std::any::Any>, args: &[Box<dyn std::any::Any>]) -> Result<Box<dyn std::any::Any>, String> {
-                            #(#arg_conversions)*
-                            let realobj = obj.downcast_ref::<#type_path>().expect("Failed to downcast to correct type");
-                            let result = realobj.#method_name(#(#arg_names),*);
-                            #return_statement
+                            fn clone_boxed(&self) -> Box<dyn Method> {
+                                Box::new(self.clone())
+                            }
                         }
 
-                        fn arg_types(&self) -> &[std::any::TypeId] {
-                            &self._arg_types
+                        /// auto-registration function
+                        #[ctor::ctor]
+                        fn #register_ident() {
+                            ::reflect::register_method::<#short_type_name>(Box::new(#method_impl_name {
+                                _name: stringify!(#method_name).to_string(),
+                                _arg_types: vec![#(#arg_types),*]
+                            }));
                         }
-                        fn return_type(&self) -> std::any::TypeId {
-                            std::any::TypeId::of::<#return_type>()
-                        }
+                    })
+                }
 
-                        fn name(&self) -> &String {
-                            &self._name
-                        }
-
-                        fn clone_boxed(&self) -> Box<dyn Method> {
-                            Box::new(self.clone())
-                        }
-                    }
-
-                    /// auto-registration function
-                    #[ctor::ctor]
-                    fn #register_ident() {
-                        ::reflect::register_method::<#short_type_name>(Box::new(#method_impl_name {
-                            _name: stringify!(#method_name).to_string(),
-                            _arg_types: vec![#(#arg_types),*]
-                        }));
-                    }
-                })
+                // temporarily do not implement static functions
+                FunctionType::Static => None
             }
         } else {
             None
