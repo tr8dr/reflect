49ed51df865b174eb4b879948dcd51e637be806d
jshore <jonathan.shore@gmail.com>
2024-08-14 17:07:53 -0400

    implementing parser

--- reflect/src/parser/parser.rs
+++ reflect/src/parser/parser.rs
@@ -1,5 +1,7 @@
 use pest::{Parser};
 use pest_derive::Parser;
+use std::any::Any;
+use std::vec::Vec;
 
 
 // Define the parser struct using the grammar file
@@ -8,6 +10,135 @@ use pest_derive::Parser;
 pub struct CTorParser;
 
 
+/// Parser for ctor expressions
+impl CTorParser {
+
+    /// Create type based on ctor expression
+    /// ```
+    ///    // create object based on ctor expression
+    ///    let obj = CTorParser::create ("Resample(Momentum(SMA,[200,560,10],0.9), 300)");
+    /// ```
+    ///
+    /// # Parameters
+    /// - `expr`: ctor expression
+    ///
+    /// # Returns
+    /// - create object instance or None
+    pub fn create (expr: &str) -> Result<Box<dyn Any>,String> {
+        todo!()
+    }
+
+    // private implementation
+
+    /// Parse ctor
+    /// - parse each argument recursively
+    /// - create object
+    ///
+    /// # Arguments
+    /// - `tree`: AST at current level
+    fn parse_ctor (tree: &pest::iterators::Pair<Rule>) -> Result<Box<dyn Any>,String> {
+        let mut argv_opt: Option<Vec::<Box<dyn Any>>> = None;
+        let mut ctor_opt: Option<&str> = None;
+
+        for subtree in tree.clone().into_inner() {
+            match subtree.as_rule() {
+                Rule::identifier => {
+                    ctor_opt = Some(subtree.as_str());
+                }
+                Rule::argument_list => {
+                    argv_opt = Self::parse_arguments (subtree.clone().into_inner());
+                }
+                _ => ()
+            }
+        }
+
+        match (ctor_opt, argv_opt) {
+            (None, _) =>
+                Err(format!("failed to parse ctor for: {}", tree.to_string())),
+            (_, None) =>
+                Err(format!("failed to parse arguments for: {}", tree.to_string())),
+            (Some(ctor), Some(argv)) => {
+                todo!()
+            }
+        }
+    }
+
+
+    /// Parse arguments
+    /// - parse each argument recursively
+    ///
+    /// # Arguments
+    /// - `tree`: AST at current level
+    fn parse_arguments (tree: pest::iterators::Pairs<Rule>) -> Option<Vec<Box<dyn Any>>> {
+        let mut argv = Vec::<Box<dyn Any>>::new();
+
+        for subtree in tree {
+            match subtree.as_rule() {
+                Rule::ctor_expression => {
+                    match Self::parse_ctor (&subtree) {
+                        Ok(obj) => argv.push(obj),
+                        Err(_) => return None
+                    }
+                }
+                Rule::identifier => {
+                    argv.push(Box::new(subtree.to_string()) as Box<dyn Any>);
+                }
+                Rule::integer => {
+                    let s = subtree.as_str();
+                    let v: i64 = str::parse::<i64>(s).unwrap();
+                    argv.push (Box::new(v));
+                }
+                Rule::float => {
+                    let s = subtree.as_str();
+                    let v = str::parse::<f64>(s).unwrap();
+                    argv.push (Box::new(v));
+                }
+                Rule::list => {
+                    argv.push (Self::parse_list (&subtree.into_inner()));
+                }
+                _ => ()
+            }
+        }
+
+        Some(argv)
+    }
+
+
+    /// Parse arguments
+    /// - parse each argument recursively
+    ///
+    /// # Arguments
+    /// - `tree`: AST at current level
+    fn parse_list (tree: &pest::iterators::Pairs<Rule>) -> Box<dyn Any> {
+        let mut fvec = Vec::<f64>::new();
+        let mut ivec = Vec::<i32>::new();
+
+        for subtree in tree.clone() {
+            match subtree.as_rule() {
+                Rule::integer => {
+                    let s = subtree.as_str();
+                    let v = str::parse::<i32>(s).unwrap();
+                    ivec.push (v);
+                    fvec.push (v as f64);
+                }
+                Rule::float => {
+                    let s = subtree.as_str();
+                    let v = str::parse::<f64>(s).unwrap();
+                    fvec.push (v);
+                    ivec.clear();
+                }
+                _ => ()
+            }
+        }
+
+        if fvec.len() > ivec.len() {
+            Box::new(fvec) as Box<dyn Any>
+        } else{
+            Box::new(ivec) as Box<dyn Any>
+        }
+    }
+
+}
 
 #[cfg(test)]
 mod tests {
